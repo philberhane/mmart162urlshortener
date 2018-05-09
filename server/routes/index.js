9a41e07486c32309a3b72535651f9c0b07931f32
Philemon Berhane <philberhane@yahoo.com>
2018-05-08 19:43:54 -0700

    Established Application Logic

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,14 +3,30 @@ const database = []
 module.exports = {
         postForm(req, res) {
         console.log(req.body)
-        database.push(req.body)
-
-        res.status(200).send({
-          message : `Here is your code: ${req.body.code}`,
-          //Define ask sarah
-          database: database
-        })
-        console.log(database)
+        
+            
+            if (req.body.code.length === 0) {
+                const alphabet = 'abcdefghijklmnopqrstuvwxyz'
+                let randomGeneratedCode = ''
+       
+                    for (i = 0; i < 6; i++) {
+                        randomGeneratedCode += alphabet.charAt(Math.floor(Math.random() * alphabet.length))
+                        }
+                  
+                  req.body.code = randomGeneratedCode
+                    
+            }           
+            
+            else if (req.body.code.length > 0 && req.body.code.length < 6) {
+                res.status(500).send({
+                message: 'Your code is too short! Please enter a 6 letter code'
+                })
+            }
+    
+            res.status(200).send({
+                message : `Here is your code: ${req.body.code}`,
+            })
+        
     }
 
 }
