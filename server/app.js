787ba751b7f2e30a52b920a0868355daaf4b7297
Amy Chew <amychew@Amys-MacBook-Pro.local>
2018-05-22 19:51:23 -0700

    Fix spacing in between lines

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ const mongodb = require('mongodb')
 const mongoose = require('mongoose')
 var mongoDB = 'insert_your_database_url_here';
 
-//create database connection
+//Create database connection
 const DATABASE_NAME = 'url_database'
 const MONGODB_URI = 'mongodb://localhost:27017/' + DATABASE_NAME
 mongoose.connect(MONGODB_URI)
@@ -18,32 +18,17 @@ db.on('error', console.error.bind(console, 'MongoDB connection error'))
 
 let app = express()
 app.use(express.static('../client'))
-// Middleware: Does stuff to the request and response objects
-// before routing:
+// Middleware executes before routing
 app.use(bodyParser.json())
 app.use(logger('dev'))
 app.use(errorhandler())
 app.use(cors())
 
-
-
 app.post('/response', routes.postForm)
-
 app.get('/', function (req, res) {
     const path = require('path')
     res.sendFile(path.resolve('../client/index.html'))
-})
-
-
-app.get('/:code', routes.redirectUrl) 
-
-
-/*
-// NOTE: We will worry about comments next week:
-app.get('/posts/:postId/comments', routes.comments.getComments)
-app.post('/posts/:postId/comments', routes.comments.addComment)
-app.put('/posts/:postId/comments/:commentId', routes.comments.updateComment)
-app.delete('/posts/:postId/comments/:commentId', routes.comments.removeComment)
-*/
+    })
+app.get('/:code', routes.redirectUrl)
 
 app.listen(3000)
--- server/models/index.js
+++ server/models/index.js
@@ -1,11 +1,9 @@
 const mongoose = require('mongoose')
 
-//Define a schema
 const ShortenerSchema = new mongoose.Schema({
     date_expires: Date,
     longUrl: String,
     code: String,
-
 })
 
 module.exports = mongoose.model('Shortener', ShortenerSchema);
